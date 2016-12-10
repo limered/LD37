2097211bf34f98bcb03808c725680db757f7c7cd
limered <me@limered.de>
2016-12-10 20:49:42 +0100

    #9 Enemy Movement System added

---
+++ Assets/Systems/EnemyMovementSystem/Components/EnemyMovementComponent.cs
@@ -0,0 +1,13 @@
+﻿using UniRx;
+using UnityEditor;
+
+namespace Assets.Systems.EnemyMovementSystem.Components
+{
+    public class EnemyMovementComponent : GameComponent
+    {
+        public bool IsOnWall { get; set; }
+        public bool IsActive = true;
+
+        public Vector3ReactiveProperty TargetPosition;
+    }
+}
