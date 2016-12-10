5ae485ef59276bc7c4d1ec8d887cf2a0f7ad928e
limered <me@limered.de>
2016-12-10 23:23:18 +0100

    #11 add death of enemy system

---
+++ Assets/Systems/EnemyDeathSystem/Components/EnemyDeathOnFallConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UniRx;
+
+namespace Assets.Systems.EnemyDeathSystem.Components
+{
+    public class EnemyDeathOnFallConfig : GameComponent
+    {
+        public FloatReactiveProperty FallThreshold = new FloatReactiveProperty();
+    }
+}
