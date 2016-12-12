d320b08244c663481aba0fa7728dbec7089489e8
limered <me@limered.de>
2016-12-12 11:40:31 +0100

    #33 game commands helper

---
+++ Assets/Systems/GameCommands/GameCommandsSystem.cs
@@ -0,0 +1,32 @@
+﻿using Assets.Scripts.Utils;
+using Assets.Systems.GameCommands.Components;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Assets.Systems.GameCommands
+{
+    public class GameCommandsSystem : IGameSystem
+    {
+        public int Priority { get { return 1; } }
+        public List<Type> SystemComponents { get { return new List<Type> { typeof(GameCommandsHelper) }; } }
+
+        public void Init()
+        {
+        }
+
+        public void RegisterComponent(IGameComponent component)
+        {
+            RegisterComponent(component as GameCommandsHelper);
+        }
+
+        public void RegisterComponent(GameCommandsHelper helper)
+        {
+            if (!helper) return;
+            if (helper.EndGameButton.WasPressed())
+                Debug.WriteLine("Close game");
+            if (helper.StartGameButton.WasPressed())
+                Debug.WriteLine("Start game");
+        }
+    }
+}
